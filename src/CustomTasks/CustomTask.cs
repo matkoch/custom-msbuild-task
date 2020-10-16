df9cf2f9b6ed73ad0eec6ac1ef5262ba6a8a8a1b
Matthias Koch <ithrowexceptions@gmail.com>
2020-10-16 23:33:25 +0200

    Extend task input and output values

--- src/CustomTasks/CustomTask.cs
+++ src/CustomTasks/CustomTask.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
 
 namespace CustomTasks
 {
@@ -7,10 +9,15 @@ namespace CustomTasks
     {
         [Required] public string StringParameter { get; set; }
 
-        public ITaskItem[] FilesParameter { get; set; }
+        public ITaskItem[] FileItems { get; set; }
+
+        [Output] public string OutputParameter { get; set; }
+        [Output] public ITaskItem[] OutputItems { get; set; }
 
         protected override bool ExecuteInner()
         {
+            OutputParameter = StringParameter;
+            OutputItems = new[] {new TaskItem($"{StringParameter}.cs")}.Concat(FileItems).ToArray();
             return true;
         }
     }
