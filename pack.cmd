4342c719c20d2473529137b0a5260b917cfdcb15
matt <ithrowexceptions@gmail.com>
2022-10-18 19:01:32 +0200

    Update TFMs

--- pack.cmd
+++ pack.cmd
@@ -1,3 +1,3 @@
-dotnet publish ./src/CustomTasks --framework netcoreapp2.1
+dotnet publish ./src/CustomTasks --framework netcoreapp6.0
 dotnet publish ./src/CustomTasks --framework net472
 dotnet pack ./src/MainLibrary --output ./
