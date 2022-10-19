a137554d109f0336f0b1a1b9170fcaafb6426e3a
Matthias Koch <ithrowexceptions@gmail.com>
2022-10-19 21:40:46 +0200

    Add warning

--- src/CustomTasks/CustomTask.cs
+++ src/CustomTasks/CustomTask.cs
@@ -8,7 +8,6 @@ namespace CustomTasks;
 public class CustomTask : ContextAwareTask
 {
     [Required] public string StringParameter { get; set; }
-
     public ITaskItem[] FileItems { get; set; }
 
     [Output] public string OutputParameter { get; set; }
@@ -17,6 +16,18 @@ public class CustomTask : ContextAwareTask
     protected override bool ExecuteInner()
     {
         string GetFile([CallerFilePath] string path = null) => path;
+
+        Log.LogWarning(
+            subcategory: "subcategory",
+            warningCode: "CT1337",
+            helpKeyword: "helpKeyword",
+            helpLink: "helpLink",
+            file: GetFile(),
+            lineNumber: 100,
+            columnNumber: 5,
+            endLineNumber: 0,
+            endColumnNumber: 0,
+            message: "message");
 
         BuildEngine.LogWarningEvent(
             new BuildWarningEventArgs(
